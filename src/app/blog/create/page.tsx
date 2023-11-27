fd58c13f9c62cd6a9de657c1b11094c7a57a020c
Chinzorigo <chinzorig.o@gmail.com>
2023-11-28 00:38:42 +0800

    added shadcn form and zod

---
+++ src/app/blog/create/page.tsx
@@ -0,0 +1,10 @@
+import BlogForm from "@/components/blog/Form";
+import { FunctionComponent } from "react";
+
+interface CreateProps {}
+
+const Create: FunctionComponent<CreateProps> = () => {
+  return <BlogForm />;
+};
+
+export default Create;
---
+++ src/app/blog/edit/[id]/page.tsx
@@ -0,0 +1,14 @@
+import BlogForm from "@/components/blog/Form";
+import { FunctionComponent } from "react";
+
+interface EditProps {
+  params: {
+    id: string;
+  };
+}
+
+const Edit: FunctionComponent<EditProps> = ({ params: { id } }) => {
+  return <BlogForm id={id} />;
+};
+
+export default Edit;
