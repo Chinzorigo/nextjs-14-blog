497d40192436d07ab6c849466d8132bb8737b318
Chinzorigo <chinzorig.o@gmail.com>
2023-11-28 00:45:47 +0800

    removed ' sign from not-found pages

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div>
       <h1>Энэ хуудас олдсонгүй</h1>
-      <p>Couldn't find requested resource</p>
+      <p>Couldnt find requested resource</p>
       <p>
         View <Link href="/blog"> All posts </Link>
       </p>
